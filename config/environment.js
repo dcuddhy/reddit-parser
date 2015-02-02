06db0b3250cd563771882ba8cf0711cbc59c6560
dcuddhy <dcuddhy@gmail.com>
2015-02-02 13:52:45 -0700

    well that's done

--- config/environment.js
+++ config/environment.js
@@ -13,6 +13,16 @@ module.exports = function(environment) {
       }
     },
 
+  contentSecurityPolicy: {
+    'default-src': "'none'",
+    'script-src': "'self' 'unsafe-eval'",
+    'font-src': "'self' fonts.gstatic.com",
+    'connect-src': "'self' localhost:3000 ws://localhost:35729 ws://0.0.0.0:35729 http://www.reddit.com/",
+    'img-src': "'self' http://www.stormfans.de",
+    'style-src': "'self' 'unsafe-inline' fonts.googleapis.com",
+    'media-src': "'self'"
+  },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
